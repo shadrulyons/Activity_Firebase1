ef333447ff212350d2aeac353f10081a78939555
shadrulyons <shadrul.fuadi.2016@ft.umy.ac.id>
2020-05-13 22:40:11 +0700

    Activity Firebase1

---
+++ app/src/main/java/com/example/testbarang1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.testbarang1;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bTambah;
+    private Button bLihat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bTambah = (Button) findViewById(R.id.btnTambah);
+        bLihat = (Button) findViewById(R.id.btnLihat);
+
+        bTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TambahData.getActIntent(MainActivity.this));
+            }
+        });
+        bLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
